bb483a5e692e51b3bc3926bb28d66b8298da0120
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-10-11 10:48:00 -0500

    added count method

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+        text: 0
+    }
+  }
+
+
+
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.count.bind(this)}></textarea>
+        < div className = "counter" > {this.state.text} </div>
       </div>
     );
+
   }
+
+
+  count(event) {
+    this.setState({
+
+      text: event.target.value.length
+
+    });
+  }
+
 }
 
 export default App;
